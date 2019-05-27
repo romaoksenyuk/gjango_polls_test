75998948dd632fad997eec5c070e5f8387b97da7
romaoksenuk <roma.oksenuk@gmail.com>
2019-05-27 15:48:12 +0300

    Testing app 'polls'

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
+import datetime
+from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+    question_text = models.CharField(max_length=205)
+    pub_date = models.DateTimeField('Дата публикации')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=205)
     votes = models.IntegerField(default=0)
+
